57eaf2247ad1b44368c4f185c098c9f310280534
David Reger <reger1@quartz1922.llnl.gov>
2024-01-09 11:31:47 -0800

    Diablo Mesh Updates

--- include/base/CardinalEnums.h
+++ include/base/CardinalEnums.h
@@ -25,6 +25,7 @@ MooseEnum getSynchronizationEnum();
 MooseEnum getNekOrderEnum();
 MooseEnum getBinnedVelocityComponentEnum();
 MooseEnum getNekFieldEnum();
+MooseEnum getDiabloFieldEnum();
 MooseEnum getOperationEnum();
 MooseEnum getTallyTypeEnum();
 MooseEnum getTallyEstimatorEnum();
@@ -101,6 +102,18 @@ enum NekFieldEnum
   unity
 };
 
+/// Enumeration of possible fields to read from diablo
+enum DiabloFieldEnum
+{
+  disp_x,
+  disp_y,
+  disp_z,
+  vel_x,
+  vel_y,
+  vel_z,
+  testing
+};
+
 /// Enumeration of possible fields to write in nekRS
 enum NekWriteEnum
 {
@@ -112,6 +125,14 @@ enum NekWriteEnum
   mesh_velocity_x,
   mesh_velocity_y,
   mesh_velocity_z
+};
+
+enum DiabloWriteEnum
+{
+  pres,
+  tr_x,
+  tr_y,
+  tr_z
 };
 } // namespace field
 
